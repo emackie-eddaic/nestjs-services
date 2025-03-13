6501cdd865e20ec4f73b65f481e2cff82ce17a6b
Eric Mackie <eric.mackie@essolutions.ca>
2025-03-13 18:00:55 -0400

    fix(eddaic/nestjs-cacheable): add `lookup` function back

--- packages/nestjs-cacheable/src/cacheable.service.ts
+++ packages/nestjs-cacheable/src/cacheable.service.ts
@@ -4,4 +4,8 @@ import CacheableLookup from 'cacheable-lookup';
 @Injectable()
 export class CacheableService {
   public readonly cacheable = new CacheableLookup();
+  lookup() {
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    return this.cacheable.lookup;
+  }
 }
