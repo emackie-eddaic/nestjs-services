bd18463404712d4d32aa5ed761f665bacf070bda
Eric Mackie <eric.mackie@essolutions.ca>
2025-07-14 11:06:05 -0400

    fix(nestjs-decorators): missing export

--- packages/nestjs-decorators/src/string/transformer/index.ts
+++ packages/nestjs-decorators/src/string/transformer/index.ts
@@ -1,4 +1,5 @@
 export * from './split-string.transformer';
 export * from './to-boolean.transformer';
 export * from './to-lower.transformer';
+export * from './to-null.transformer';
 export * from './trim.transformer';
